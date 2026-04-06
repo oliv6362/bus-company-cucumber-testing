91857b78a4e669b30f4559ca534aa7333e84d604
oliv6362 <33200860+oliv6362@users.noreply.github.com>
2026-04-06 15:49:18 +0200

    test: added step definition for the feature

---
+++ BusCompany.Specs/StepDefinitions/PriceCalculatorStepDefinitions.cs
@@ -0,0 +1,24 @@
+
+namespace BusCompany.Specs.StepDefinitions;
+
+[Binding]
+public sealed class PriceCalculatorStepDefinitions
+{
+    [Given("the distance is {int}")]
+    public void GivenTheDistanceIs(int number)
+    {
+        throw new PendingStepException();
+    }
+
+    [When("the price has been calculated")]
+    public void WhenThePriceHasBeenCalculated()
+    {
+        throw new PendingStepException();
+    }
+
+    [Then("the price should be {int}")]
+    public void ThenThePriceShouldBe(int result)
+    {
+        throw new PendingStepException();
+    }
+}
