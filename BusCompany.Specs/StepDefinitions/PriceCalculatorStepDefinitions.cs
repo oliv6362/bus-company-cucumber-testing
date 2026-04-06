d2b0f3853823cf9444119ebd98267d592d99fee5
oliv6362 <33200860+oliv6362@users.noreply.github.com>
2026-04-06 19:57:27 +0200

    test: added the implementation for the step definitions

--- BusCompany.Specs/StepDefinitions/PriceCalculatorStepDefinitions.cs
+++ BusCompany.Specs/StepDefinitions/PriceCalculatorStepDefinitions.cs
@@ -4,27 +4,41 @@ namespace BusCompany.Specs.StepDefinitions;
 [Binding]
 public sealed class PriceCalculatorStepDefinitions
 {
+    private readonly PriceCalculator _priceCalculator = new PriceCalculator();
+    private int _distance;
+    private decimal _calculatedPrice;
+    private Exception? _exception;
+    
     [Given("the distance is {int}")]
-    public void GivenTheDistanceIs(int number)
-    {
-        throw new PendingStepException();
+    public void GivenTheDistanceIs(int distance)
+    { 
+        _distance = distance;
     }
 
     [When("the price has been calculated")]
     public void WhenThePriceHasBeenCalculated()
     {
-        throw new PendingStepException();
+        try
+        {
+            _calculatedPrice = _priceCalculator.CalculatePrice(_distance);
+        }
+        catch (Exception ex)
+        {
+            _exception = ex;
+        }
     }
 
     [Then("the price should be {int}")]
-    public void ThenThePriceShouldBe(int result)
+    public void ThenThePriceShouldBe(int price)
     {
-        throw new PendingStepException();
+        Assert.Null(_exception);
+        Assert.Equal(price, _calculatedPrice);
     }
 
     [Then("the distance should be rejected")]
     public void ThenTheDistanceShouldBeRejected()
     {
-        throw new PendingStepException();
+        Assert.NotNull(_exception);
+        Assert.IsType<ArgumentException>(_exception);
     }
 }
